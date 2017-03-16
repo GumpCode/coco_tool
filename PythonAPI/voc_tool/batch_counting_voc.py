a8b37d06d6f25bfba86e3302dd96d39f9a855a84
GumpCode <GumpCode@github.com>
2017-03-16 21:52:44 +0800

    add voc bboxes counting

--- PythonAPI/voc_tool/batch_counting_voc.py
+++ PythonAPI/voc_tool/batch_counting_voc.py
@@ -1,3 +1,16 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# **********************************************************
+# * Author        : Gump from CQU
+# * Email         : gumpglh@qq.com
+# * Create time   : 2017-03-16 03:54
+# * Last modified : 2017-03-16 03:54
+# * Filename      : batch_counting_voc.py
+# * Description   :
+# * Copyright © gumpglh. All rights reserved.
+# **********************************************************
+
 import os
 import subprocess
 import sys
@@ -13,8 +26,8 @@ CAFFE_ROOT = "{}/GumpCode/ssd".format(HOMEDIR)
 # The root directory which stores the coco images, annotations, etc.
 voc_data_dir = "/home/ganlinhao/voc/VOCdevkit"
 # The sets that we want to get the size info.
-anno_sets = ["VOC2007","VOC2012"]
-data_types = ["val", "train"]
+anno_sets = ["VOC2007","VOC2012", "VOC2007test"]
+data_types = ["val", "train", "test"]
 # The directory which stores the image id and size info.
 anno_out_dir = "/data/MixedData/Annotations"
 img_out_dir = "/data/MixedData/ImageSets"
@@ -29,12 +42,14 @@ for i in xrange(0, len(anno_sets)):
     # The directory which contains the full jpeg imgages for each set.
     jpeg_dir = "{}/{}/JPEGImages".format(voc_data_dir, anno_set)
     # The directory which stores the imageset information for each set.
-    imgset_dir = "{}/ImageSets/Main".format(voc_data_dir)
+    imgset_dir = "{}/{}/ImageSets/Main".format(voc_data_dir, anno_set)
     for j in range(len(data_types)):
-        data_type = data_types[i]
-        imagset_file = "{}/{}.txt".format(anno_dir, data_type)
-        if not os.path.exists(anno_file):
+        data_type = data_types[j]
+        imgset_file = "{}/{}.txt".format(imgset_dir, data_type)
+        if os.path.exists(imgset_file) is not True:
             continue
+        #if not os.path.exists(anno_file):
+        #    continue
         #anno_name = anno_set.split("_")[-1]
         #imgset_file = "{}/{}.txt".format(imgset_dir, anno_name)
         if not os.path.exists(imgset_file):
@@ -42,9 +57,11 @@ for i in xrange(0, len(anno_sets)):
             sys.exit()
         #name_size_file = "{}/{}_name_size.txt".format(out_dir, anno_name)
         if redo or not os.path.exists(anno_out_dir):
-            cmd = "python {}/counting_voc.py {} {} {} {} {}" \
-                    .format(CURDIR, anno_dir, imgset_file, data_type, anno_out_dir, img_out_dir)
+            cmd = "python {}/counting_voc.py {} {} {} {} {} {}" \
+                    .format(CURDIR, anno_dir, imgset_file, anno_set, data_type, anno_out_dir, img_out_dir)
             print cmd
             process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
             output = process.communicate()[0]
             print output
+            #if redo is True:
+            #    sys.exit()
--- PythonAPI/voc_tool/counting_voc.py
+++ PythonAPI/voc_tool/counting_voc.py
@@ -1,52 +1,105 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# **********************************************************
+# * Author        : Gump from CQU
+# * Email         : gumpglh@qq.com
+# * Create time   : 2017-03-14 23:28
+# * Last modified : 2017-03-16 03:54
+# * Filename      : counting_voc.py
+# * Description   :
+# * Copyright © gump. All rights reserved.
+# **********************************************************
+
+import argparse
+from collections import OrderedDict
+import json
 import os
-import subprocess
 import sys
+import shutil
+sys.path.append(os.path.dirname(sys.path[0]))
 
-HOMEDIR = os.path.expanduser("~")
-CURDIR = os.path.dirname(os.path.realpath(__file__))
+from pycocotools.coco import COCO
+from pycocotools.json2xml import XmlWriter, XmlReader
 
-### Modify the address and parameters accordingly ###
-# If true, redo the whole thing.
-redo = True
-# The caffe root.
-CAFFE_ROOT = "{}/GumpCode/ssd".format(HOMEDIR)
-# The root directory which stores the coco images, annotations, etc.
-coco_data_dir = "/data/coco"
-# The sets that we want to get the size info.
-anno_sets = ["instances_val2014","instances_train2014"]
-data_types = ["val", "train"]
-#anno_sets = ["image_info_test-dev2015", "instances_val2014", "image_info_test2014",
-#        "image_info_test2015", "instances_train2014"]
-#anno_sets = anno_sets + ["instances_minival2014", "instances_valminusminival2014"]
-# The directory which contains the full annotation files for each set.
-anno_dir = "{}/annotations".format(coco_data_dir)
-# The directory which stores the imageset information for each set.
-imgset_dir = "{}/ImageSets".format(coco_data_dir)
-# The directory which stores the image id and size info.
-#out_dir = "{}/data/coco".format(CAFFE_ROOT)
-anno_out_dir = "/data/MixedData/Annotations"
-img_out_dir = "/data/MixedData/ImageSets"
-if not os.path.exists(anno_out_dir):
-    os.makedirs(anno_out_dir)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description = "Get the image size from an annotation file.")
+    parser.add_argument("anno_dir",
+            help = "The file which contains all the annotations for a dataset in json format.")
+    parser.add_argument("imgsetfile", default = "",
+            help = "A file which contains the image set information.")
+    parser.add_argument("anno_set", default = "",
+            help = "the name of dataset.")
+    parser.add_argument("data_type", default = "",
+            help = "Specify the output datatpy, eg:val/train.")
+    parser.add_argument("anno_out_dir", default = "",
+            help = "A dir which stores the new xml file.")
+    parser.add_argument("img_out_dir", default = "",
+            help = "A dir which stores the new img file.")
 
-### Get image size info ###
-for i in xrange(0, len(anno_sets)):
-    anno_set = anno_sets[i]
-    data_type = data_types[i]
-    anno_file = "{}/{}.json".format(anno_dir, anno_set)
-    if not os.path.exists(anno_file):
-        continue
-    anno_name = anno_set.split("_")[-1]
-    imgset_file = "{}/{}.txt".format(imgset_dir, anno_name)
-    if not os.path.exists(imgset_file):
-        print "{} does not exist".format(imgset_file)
+    args = parser.parse_args()
+    anno_dir = args.anno_dir
+    imgsetfile = args.imgsetfile
+    data_type = args.data_type
+    anno_set = args.anno_set
+    anno_out_dir = args.anno_out_dir
+    img_out_dir = args.img_out_dir
+
+    """
+    #######coco map to voc(id to name)#########
+    class_map = {5:"aeroplane", 2:"bicycle", 15:"bird", 9:"boat", 40:"bottle", 6:"bus",
+            3:"car", 16:"cat", 57:"chair", 20:"cow", 61:"diningtable", 17:"dog", 18:"horse",
+            4:"motorbike", 1:"person", 59:"pottedplant", 19:"sheep", 58:"sofa", 7:"train",
+            63:"tvmonitor"}
+    #######coco map to voc(id to id)##########
+    catId_map = {5:1, 2:2, 15:3, 9:4, 40:5, 6:6, 3:7, 16:8,
+            57:9, 20:10, 61:11, 17:12, 18:13, 4:14, 1:15, 59:16,
+            19:17, 58:18, 7:19, 63:20}
+    """
+    class_map = {"background":0, "aeroplane":1, "bicycle":2, "bird":3, "boat":4, "bottle":5,
+        "bus":6, "car":7, "cat":8, "chair":9, "cow":10, "diningtable":11, "dog":12, "horse":13,
+        "motorbike":14, "person":15, "pottedplant":16, "sheep":17, "sofa":18, "train":19,
+        "tvmonitor":20}
+    depth = 3
+    bboxcount_list = [0 for i in range(len(class_map.keys()))]
+
+
+    dirname = imgsetfile.split('.')[0]
+
+    if not os.path.exists(imgsetfile):
+        print "{} does not exist".format(imgsetfile)
         sys.exit()
-    #name_size_file = "{}/{}_name_size.txt".format(out_dir, anno_name)
-    if redo or not os.path.exists(anno_out_dir):
-        cmd = "python {}/json2xml_coco.py {} {} {} {} {}" \
-                .format(CURDIR, anno_file, imgset_file, data_type, anno_out_dir, img_out_dir)
-                #all.json train/val.txt output_dir
-        print cmd
-        process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
-        output = process.communicate()[0]
-        print output
+
+    if not os.path.exists(anno_out_dir):
+        os.makedirs(anno_out_dir)
+    if anno_out_dir:
+        #get annotation info
+        with open(imgsetfile, 'r') as f:
+            for line in f.readlines():
+                anno_file = "{}/{}.xml".format(anno_dir, line.strip('\n'))
+                anns = XmlReader(anno_file)
+                #print anns.getShapes()
+                for anno in anns.getShapes():
+                    #print bboxcount_list[class_map[anno[0]]]
+                    bboxcount_list[class_map[anno[0]]] += 1
+
+#            for ann in anns:
+#                cat_id = ann['category_id']
+#                if cat_id not in class_map.keys():
+#                    continue
+#                bbox = ann['bbox']
+#                #count the bbox number per class
+#                bboxcount_list[catId_map[cat_id] -1] += 1
+#
+#
+#    src = "{}/{}".format(dirname, file_name)
+#    dst_dir = "{}/{}".format(img_out_dir, data_type)
+#    if not os.path.exists(dst_dir):
+#        os.makedirs(dst_dir)
+#    dst_path = "{}/{}".format(dst_dir, file_name)
+#    shutil.copy(src, dst_path)
+#
+    output_txt = "{}_{}.txt".format(anno_set, data_type)
+    with open(output_txt, "w") as f:
+        for i in range(len(bboxcount_list)):
+            f.write(str(bboxcount_list[i]) + "\n")
